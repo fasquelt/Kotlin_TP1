517d36e05aaff63777f119d91b1cca2cad518d42
Tom Fasquelle <tom.fasquelle@etu.univ-lyon1.fr>
2024-05-06 16:43:39 +0200

    Optimisation des imports

--- app/src/main/java/com/example/tp1/AjoutContact.kt
+++ app/src/main/java/com/example/tp1/AjoutContact.kt
@@ -2,13 +2,8 @@ package com.example.tp1
 
 import android.content.Intent
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
-import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.tp1.databinding.ActivityAjoutBinding
 
 class AjoutContact : AppCompatActivity() {
