86c57293cd5bfb67d760f5a26489eba15d56d701
Tom Fasquelle <tom.fasquelle@etu.univ-lyon1.fr>
2024-05-14 22:10:52 +0200

    Test d'implémentation de List

--- app/src/main/java/com/example/tp1/MyListAdapter.kt
+++ app/src/main/java/com/example/tp1/MyListAdapter.kt
@@ -5,8 +5,19 @@ import android.app.Activity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-private class MyListAdapter(private val context: Activity, contacts: Array<Contact>)
-    : ArrayAdapter<Contact>(context, R.layout.custom_list, contacts) {
+private class MyListAdapter(private val context: Activity, val contacts: Array<Contact>)
+    : BaseAdapter(){
+    override fun getCount(): Int {
+        return contacts.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return contacts[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        TODO("Not yet implemented")
+    }
 
     @SuppressLint("ViewHolder", "InflateParams")
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
